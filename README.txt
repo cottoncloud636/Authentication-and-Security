82f3e1d9689899fca180eb63ebf68d926a4ab6e5
Natasha Zhou <35412065+cottoncloud636@users.noreply.github.com>
2023-12-01 17:58:31 -0800

    level 2 environment variables

---
+++ README.txt
@@ -0,0 +1,10 @@
+https://www.npmjs.com/package/dotenv
+
+1) npm i dotenv
+
+2) import 'dotenv/config'
+
+3) create a .env file in root directory
+
+4) add my own environment variables to .env file I just created
+How to do? In level 2, I created a string "Thisisourlittlesecret.", add this string to .env file following the required format --> NAME=VALUE
---
+++ app.js
@@ -0,0 +1,157 @@
+//jshint esversion:6
+//!!!!!!!! because mongoose library has some method for encryption purpose, hence we use mongodb in this section
+//!!!!!!!! instead of PostgreSQL
+
+//step 1: install -- npm init -y
+//step 2: install -- npm i express ejs body-parser
+
+//Mongoose is an ODM (Object Data Modeling) library for MongoDB, meaning it acts as an 
+                                   //intermediary between your Node.js application and the MongoDB database
+
+//connecting to a MongoDB instance running on your local machine or on a specific 
+                        //server within your local network.
+
+// const port = 3000;
+//"userDB" is our self-defined username for this DB. "useNewUrlParser: true": removes the warning in console
+
+// app.set('view engine', 'ejs');//explicitly tells Express to use EJS (Embedded JavaScript) as the templating engine.
+
+//level 2 security -- mongoose encryption: https://www.npmjs.com/package/mongoose-encryption
+
+import express from "express";
+import bodyParser from "body-parser";
+                        
+import mongoose from "mongoose";   
+import encrypt from 'mongoose-encryption';
+import 'dotenv/config';
+// console.log(process.env.API_KEY);
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+// ... Your connection code
+async function connectToDB() {
+    try {
+      await mongoose.connect("mongodb://127.0.0.1:27017/userDB");//if I use "localhost" instead of 127.0.0.1
+                                      //for some reason, the program can't connect to mongodb
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      // Handle the error appropriately, maybe try reconnecting or terminate the app
+    }
+  }
+  
+  connectToDB(); // Call the function to establish the connection
+  
+  // ... Rest of your code with error handling for database operations
+  
+
+//######## create a mongo schema (aka. the DB structure) first
+//######## this is level 1 security mongo schema, userSchema is simply a JS obj
+/*const userSchema = {
+    email: String,
+    password: String
+};*/
+
+//######## this is level 2 security mongo schema, now userSchema is not just an JS obj, it is an obj from 
+//######## mongoose class
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+//######## https://www.npmjs.com/package/mongoose-encryption#secret-string-instead-of-two-keys
+//         using a convenient way to encryption, it's called Secret String Instead of Two Keys
+//######## however, this is still dangerous, because once project code is pushed to github, then everyone can search
+//         this string. Hence, we can use sth. called environment variables, and there is a pkg in mkt called dotenv
+//         can handle this. https://www.npmjs.com/package/dotenv
+//         1st, require dotevn as early as possible in the project. 2nd, create a .env file by typing
+//         "ni .env" in powershell.
+/*
+let secret = "Thisisourlittlesecret.";
+*/
+
+//add mongo encrypt as a plugin to our schema, and we will pass over our secret as JS obj
+//we need to add this plugin to schema before creating our mongoose model cuz we are passing userSchema as a param
+//to create our mongoose model
+//but one drawback, the following code will encrypt the entire DB, hence it encrypt emails too, but it is not necessary
+/*
+userSchema.plugin(encrypt, { secret: secret });
+*/
+//######## hence, in order to only encrypt a certain area, we modify the above code:
+//https://www.npmjs.com/package/mongoose-encryption#encrypt-only-certain-fields
+/*
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});
+*/
+//######## let's use environment variables
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+
+
+
+
+//use the schema we just created to set up a new user model
+const User = new mongoose.model("User", userSchema);  //"User" is the name of our data collection
+                                          //after this point, we can create users and add to userDB
+
+
+//display a home page, use get
+app.get ("/", (req, res)=>{
+    res.render("home.ejs");
+});
+
+//display a register page
+app.get ("/register", (req, res)=>{
+    res.render("register.ejs");
+});
+
+//display a login page
+app.get ("/login", (req, res)=>{
+    res.render("login.ejs");
+});
+
+//post the user input for email and password to /register endpoint
+/*!!!!!!!! only after user input for email and registration has no problems, then render the secrets.ejs page!!!!!!!!*/
+app.post("/register", (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    //save this new user information
+    //~~~~~~~~ level 2: in mongoose encryption, when we call save(), the psw field is automatically encrypted ~~~~~~~~*/
+    newUser.save().then(()=>{
+        res.render("secrets.ejs");
+    }).catch((err)=>{
+            console.log(err);
+        });
+    });
+
+//now, since user has registered email and psw, now they can submit their credentials to loging
+app.post("/login", (req, res)=>{
+    const email = req.body.username;
+    const psw = req.body.password;
+    //first, let's find the email from the "User" model that user submitted during registration stage
+    //~~~~~~~~ level 2: in mongoose encryption, when we call findOne(), the psw field is automatically decrypted ~~~~~~~~*/
+    User.findOne({email: email}).exec().then(foundUser=>{ //findOne() is a method provided by Mongoose, which is an Object Data Modeling (ODM) library for
+                                // MongoDB when you're using it in a Node.js environment. It's not a 
+                                //method native to JavaScript or MongoDB directly.
+                                // is used to find a single document in a collection that matches the specified criteria. It allows you to query the database for documents based on certain conditions and retrieve the first document that matches those conditions.
+        if (foundUser) {
+            if (foundUser.password === psw) {
+                res.render("secrets.ejs");
+            }
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+
+    });
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000");
+});
+
+
+
+
